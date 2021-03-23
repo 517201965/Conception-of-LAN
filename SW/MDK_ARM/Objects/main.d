345cc101cacbe465bc3f59b85e10d8bee7686a6b
JayShen <517201965@qq.com>
2021-03-23 10:33:56 +0800

    Create LAN8720 Project

---
+++ SW/MDK_ARM/Objects/main.d
@@ -0,0 +1,15 @@
+.\objects\main.o: ..\USER\main.c
+.\objects\main.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\objects\main.o: ..\Libraries\CMSIS\CM3\CoreSupport\core_cm3.h
+.\objects\main.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\main.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
+.\objects\main.o: ..\USER\stm32f10x_conf.h
+.\objects\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_fsmc.h
+.\objects\main.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\objects\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\objects\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\objects\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_spi.h
+.\objects\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_tim.h
+.\objects\main.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\misc.h
+.\objects\main.o: ..\USER\SysTick\systick.h
+.\objects\main.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
---
+++ SW/MDK_ARM/Objects/systick.d
@@ -0,0 +1,15 @@
+.\objects\systick.o: ..\USER\SysTick\systick.c
+.\objects\systick.o: ..\USER\SysTick\systick.h
+.\objects\systick.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\objects\systick.o: ..\Libraries\CMSIS\CM3\CoreSupport\core_cm3.h
+.\objects\systick.o: C:\Program Files\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\systick.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\system_stm32f10x.h
+.\objects\systick.o: ..\USER\stm32f10x_conf.h
+.\objects\systick.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_fsmc.h
+.\objects\systick.o: ..\Libraries\CMSIS\CM3\DeviceSupport\ST\STM32F10x\stm32f10x.h
+.\objects\systick.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_gpio.h
+.\objects\systick.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_rcc.h
+.\objects\systick.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_spi.h
+.\objects\systick.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\stm32f10x_tim.h
+.\objects\systick.o: ..\Libraries\STM32F10x_StdPeriph_Driver\inc\misc.h
+.\objects\systick.o: ..\USER\SysTick\stm32f10x_systick.h
